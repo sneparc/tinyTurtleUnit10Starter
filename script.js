4615fee18f82123c46faa0514638cf46427f2d89
Casey Snepar <sneparc@gmail.com>
2016-12-30 21:35:49 +0000

    created new triangle function

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ right(90);
 forward(50);
 }
 
+function triangle(){
+    forward(50);
+    right(120);
+    forward(50);
+     right(120);
+    forward(50);
+};
 // Type your function call below
-
-
+triangle();
 stamp();
